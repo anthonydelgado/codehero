332a4c0b91b2a773f2a3af087fa57cc1edc8e83a
Anthony Delgado <me@anthonydelgado.me>
2017-01-24 18:27:10 -0500

    mayday magic

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -25,3 +25,31 @@ $.ajax({
 $(document).on('click','.click-to-call', function () {
     console.log($(this).attr('data-user'));
 });
+
+
+
+$(document).on('click','#helpme', function () {
+
+
+    $.ajax({url: "/slack-hook", success: function(result){
+        console.log(result);
+    }});
+
+    var video = document.querySelector("#videoElement");
+
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({video: true}, handleVideo, videoError);
+    }
+
+    function handleVideo(stream) {
+        video.src = window.URL.createObjectURL(stream);
+        console.log('video time')
+    }
+
+    function videoError(e) {
+        // do something
+    }
+
+});
