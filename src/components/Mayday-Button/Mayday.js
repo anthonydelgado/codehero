b1703f6346ebdc611a40bc75ecdfa94576e3f3ad
Godfrey Best <gcb246@stern.nyu.edu>
2017-01-23 23:22:08 -0500

    excess comments removed from mayday component

--- src/components/Mayday-Button/Mayday.js
+++ src/components/Mayday-Button/Mayday.js
@@ -6,33 +6,6 @@ import {Button} from 'react-materialize';
 import { browserHistory } from 'react-router';
 // react-router Link
 
-// class Mayday extends React.Component {
-	// constructor(props) {
- //    	super(props);
- //  	}
-
-// const Mayday = () => {
-// 		return (
-// 			<div className="mayday">
-// 				<Button onClick={ () => { yellMayday() } } waves='light'>Get Help!</Button>
-// 			</div>	
-// 		);
-	
-// 	function yellMayday() {
-// 		axios({
-// 		  method: 'post',
-// 		  url: 'https://hooks.slack.com/services/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',
-// 		  data: {
-// 		    "text":"This is a line of text.\nAnd this is another one."
-// 		  }
-// 		});
-
-// 		console.log('Mayday button clicked');
-// 	}
-// };
-
-// export { Mayday as default };
-// export default Mayday;
 
 const Mayday = () => {
 function yellMayday() {
