b2e0a7cba52d939f3ba56450af6899100bff16e3
Tatiana Marsh <tatiana.alcocer@gmail.com>
2017-01-24 14:04:52 -0500

    Mayday button

--- server/app.js
+++ server/app.js
@@ -21,7 +21,6 @@ app.get('/', (req, res) => {
 app.get('/slack-hook', function(req, res) {
 
 	var myJSONStr = 'payload= {"username": "Code Hero Bot", "icon_emoji": ":robot_face:", "channel": "#help-needed", "text": "There is a student in need of help! <http://localhost:9000|Click here to save them>"}'
-
   	var xmlhttp = new XMLHttpRequest();
     var webhook_url = 'https://hooks.slack.com/services/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';
         
